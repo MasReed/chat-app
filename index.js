89ea130d6c45e5eb47549328b1c6e2f3d807e6d7
MasReed <bluekitdrummer@gmail.com>
2021-03-23 16:24:50 -0500

    Message Methods

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Require packages
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const http = require('http')
+const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 
 // App instance
@@ -11,18 +14,65 @@ if (port == null || port == ""){
 }
 
 const app = express();
+http.Server(app);
 
 // Utilize EJS, body-parser, express
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static('public'));
 
+// Database
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(
+    dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    err => { if (err) {console.log("mongoDB connection", err); }}
+);
+const Message = mongoose.model('message', {
+    name: String,
+    message: String
+});
+
+// 
+io.on('connection', socket => {
+    console.log('a user has been connected');
+});
+
+
 // Render homepage
 app.get('', function(req, res) {
     res.render('index.ejs');
 });
+
+app.route('/messages')
+
+    .get( (req, res) => {
+        Message.find({}, (err, messages) => {
+            res.send(messages);
+        });
+    })
+
+    .post( async (req, res) => {
+        try {
+            let message = new Message(req.body);
+            let savedMessage = await message.save();
+            console.log('saved');
+            let censored = await Message.findOne({ message: 'badword'});
+
+            if (censored) await Message.remove({_id: censored.id});
+            else io.emit('message', req.body);
+            res.sendStatus(200);
+        } catch (error) {
+            res.sendStatus(500);
+            return console.error(error);
+        } finally {
+            console.log("message post called");
+        }
+    }
+);
 
 app.listen(port, () => {
     console.log("Started on port: " + port);
