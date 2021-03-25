53daed5977f4e1c30596e7a0da4afd420c3a7277
MasReed <bluekitdrummer@gmail.com>
2021-03-24 21:42:15 -0500

    Update Layout

--- index.js
+++ index.js
@@ -31,12 +31,13 @@ mongoose.connect(
     { useNewUrlParser: true, useUnifiedTopology: true },
     err => { if (err) {console.log("mongoDB connection", err); }}
 );
+
 const Message = mongoose.model('message', {
     name: String,
     message: String
 });
 
-// 
+//
 io.on('connection', socket => {
     console.log('a user has been connected');
 });
@@ -50,27 +51,28 @@ app.get('', function(req, res) {
 app.route('/messages')
 
     .get( (req, res) => {
-        Message.find({}, (err, messages) => {
-            res.send(messages);
-        });
-    })
-
-    .post( async (req, res) => {
-        try {
-            let message = new Message(req.body);
-            let savedMessage = await message.save();
-            console.log('saved');
-            let censored = await Message.findOne({ message: 'badword'});
-
-            if (censored) await Message.remove({_id: censored.id});
-            else io.emit('message', req.body);
-            res.sendStatus(200);
-        } catch (error) {
-            res.sendStatus(500);
-            return console.error(error);
-        } finally {
-            console.log("message post called");
-        }
+        // Message.find({}, (err, messages) => {
+        //     res.send(messages);
+        // });
+        res.render('messages.ejs');
+    // })
+    //
+    // .post( async (req, res) => {
+    //     try {
+    //         let message = new Message(req.body);
+    //         let savedMessage = await message.save();
+    //         console.log('saved');
+    //         let censored = await Message.findOne({ message: 'badword'});
+    //
+    //         if (censored) await Message.remove({_id: censored.id});
+    //         else io.emit('message', req.body);
+    //         res.sendStatus(200);
+    //     } catch (error) {
+    //         res.sendStatus(500);
+    //         return console.error(error);
+    //     } finally {
+    //         console.log("message post called");
+    //     }
     }
 );
 
